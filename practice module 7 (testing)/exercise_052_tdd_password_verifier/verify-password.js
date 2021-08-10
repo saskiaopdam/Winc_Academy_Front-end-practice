cbb16176734caace4da3bc3992b490d069bbcf2a
Saskia Opdam <opdamsaskia@icloud.com>
2021-08-10 22:00:13 +0200

    Add files via upload

--- practice module 7 (testing)/exercise_052_tdd_password_verifier/verify-password.js
+++ practice module 7 (testing)/exercise_052_tdd_password_verifier/verify-password.js
@@ -1,15 +1,25 @@
-const verifyPassword = function (password) {
-  const conditions = [
-    isNotNull(password),
-    hasRightLength(password),
-    hasUpperCaseCharacter(password),
-    hasLowerCaseCharacter(password),
-    hasDigit(password),
-  ];
-  const result =
-    minimumConditionsReached(conditions) && hasLowerCaseCharacter(password);
-
-  return result;
+const verifyPassword = function (conditions) {
+  //   const trueConditions = conditions.filter((bool) => bool);
+  const trueConditions = conditions.filter(Boolean);
+  //.filter(Boolean) removes values from a list which are falsey
+  if (trueConditions.length >= 3 && conditions[3] === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = verifyPassword;
+// onduidelijk: eerst nog test voor deze functie schrijven, of testen hiervoor in algemene password-verifier.test zetten?
+
+// const conditions = [
+//   isNotNull(password),
+//   hasRightLength(password),
+//   hasUpperCaseCharacter(password),
+//   hasLowerCaseCharacter(password),
+//   hasDigit(password),
+// ];
+// const result =
+//   minimumConditionsReached(conditions) && hasLowerCaseCharacter(password);
+
+// return result;
